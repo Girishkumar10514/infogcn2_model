8571954b6d3d43697b1748a2a15b8236262f1b74
Girish Kumar Arugonda <112767356+Girishkumar10514@users.noreply.github.com>
2024-03-16 15:43:23 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,221 +1,253 @@
-from . import compat
-
-import functools
-import itertools
+import argparse
+import random
+import pickle
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
 
-def is_cuda_enabled():
-    return torch.version.cuda is not None
+from torch.utils.data.sampler import Sampler
+from typing import Sized
+from tqdm import tqdm
+from torch import linalg as LA
 
-def get_cuda_version():
-    return tuple(int(x) for x in torch.version.cuda.split('.'))
+def import_class(name):
+    components = name.split('.')
+    mod = __import__(components[0])
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
 
-def is_fp_tensor(x):
-    if is_nested(x):
-        # Fast-fail version of all(is_fp_tensor)
-        for y in x:
-            if not is_fp_tensor(y):
-                return False
+
+def count_params(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
-    return compat.is_tensor_like(x) and compat.is_floating_point(x)
-
-def is_nested(x):
-    return isinstance(x, tuple) or isinstance(x, list)
-
-def should_cache(x):
-    if is_nested(x):
-        # Fast-fail version of all(should_cache)
-        for y in x:
-            if not should_cache(y):
-                return False
-        return True
-    return isinstance(x, torch.nn.parameter.Parameter) and \
-        type_string(x) == 'FloatTensor'
-
-def collect_fp_tensor_types(args, kwargs):
-    def collect_types(x, types):
-        if is_nested(x):
-            for y in x:
-                collect_types(y, types)
-        else:
-            types.add(type_string(x))
-
-    all_args = itertools.chain(args, kwargs.values())
-    types = set()
-    for x in all_args:
-        if is_fp_tensor(x):
-            collect_types(x, types)
-    return types
-
-def type_string(x):
-    return x.type().split('.')[-1]
-
-def maybe_half(x, name='', verbose=False):
-    if is_nested(x):
-        return type(x)([maybe_half(y) for y in x])
-
-    if not x.is_cuda or type_string(x) == 'HalfTensor':
-        return x
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
     else:
-        if verbose:
-            print('Float->Half ({})'.format(name))
-        return x.half()
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
-def maybe_float(x, name='', verbose=False):
-    if is_nested(x):
-        return type(x)([maybe_float(y) for y in x])
+def init_seed(seed):
+    torch.cuda.manual_seed_all(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
 
-    if not x.is_cuda or type_string(x) == 'FloatTensor':
-        return x
-    else:
-        if verbose:
-            print('Half->Float ({})'.format(name))
-        return x.float()
+def get_masked_input_and_labels(inp, mask_value=1, mask_p=0.15, mask_random_p=0.1, mask_remain_p=0.1, mask_random_s=1):
+    # BERT masking
+    inp_mask = (torch.rand(*inp.shape[:2]) < mask_p).to(inp.device)
 
-# NB: returneds casted `args`, mutates `kwargs` in-place
-def casted_args(cast_fn, args, kwargs):
-    new_args = []
-    for x in args:
-        if is_fp_tensor(x):
-            new_args.append(cast_fn(x))
-        else:
-            new_args.append(x)
-    for k in kwargs:
-        val = kwargs[k]
-        if is_fp_tensor(val):
-            kwargs[k] = cast_fn(val)
-    return new_args
+    # Prepare input
+    inp_masked = inp.clone().float()
 
-def cached_cast(cast_fn, x, cache):
-    if is_nested(x):
-        return type(x)([cached_cast(y) for y in x])
-    
+    # Set input to [MASK] which is the last token for the 90% of tokens
+    # This means leaving 10% unchanged
+    inp_mask_2mask = (inp_mask & (torch.rand(*inp.shape[:2]) < 1 - mask_remain_p).to(inp.device))
+    inp_masked[inp_mask_2mask] = mask_value # mask token is the last in the dict
 
-    if x in cache:
-        cached_x = cache[x]
-        next_functions_available = False
-        if x.requires_grad and cached_x.requires_grad:
-            if len(cached_x.grad_fn.next_functions) > 1:
-                next_functions_available = True
-            # Make sure x is actually cached_x's autograd parent.
-            if next_functions_available and cached_x.grad_fn.next_functions[1][0].variable is not x:
-                                raise RuntimeError("x and cache[x] both require grad, but x is not cache[x]'s parent.  This is likely an error.")
-    
-            # Make sure x is actually cached_x's autograd parent.
-            # if cached_x.grad_fn.next_functions[1][0].variable is not x:
+    # Set 10% to a random token
+    inp_mask_2random = inp_mask_2mask & (torch.rand(*inp.shape[:2]) < mask_random_p / (1 - mask_remain_p)).to(inp.device)
 
-        # During eval, it's possible to end up caching casted weights with
-        # requires_grad=False.  On the next training iter, if cached_x is found
-        # and reused from the cache, it will not actually have x as its parent.
-        # Therefore, we choose to invalidate the cache (and force refreshing the cast)
-        # if x.requires_grad and cached_x.requires_grad do not match.
-        #
-        # During eval (i.e. running under with torch.no_grad()) the invalidation
-        # check would cause the cached value to be dropped every time, because
-        # cached_x would always be created with requires_grad=False, while x would
-        # still have requires_grad=True.  This would render the cache effectively
-        # useless during eval.  Therefore, if we are running under the no_grad()
-        # context manager (torch.is_grad_enabled=False) we elide the invalidation
-        # check, and use the cached value even though its requires_grad flag doesn't
-        # match.  During eval, we don't care that there's no autograd-graph
-        # connection between x and cached_x.
-        if torch.is_grad_enabled() and x.requires_grad != cached_x.requires_grad:
-            del cache[x]
+    inp_masked[inp_mask_2random] = (2 * mask_random_s * torch.rand(inp_mask_2random.sum().item(), inp.shape[2]) - mask_random_s).to(inp.device)
 
-        elif x.requires_grad and cached_x.requires_grad and not next_functions_available:
-            del cache[x]
-        else:
-            return cached_x
+    # y_labels would be same as encoded_texts i.e input tokens
+    gt = inp.clone()
+    return inp_masked, gt
 
-    casted_x = cast_fn(x)
-    cache[x] = casted_x
-    return casted_x
+def random_rot_mat(bs, uniform_dist):
+    rot_mat = torch.zeros(bs, 3, 3)
+    random_values = uniform_dist.rsample((bs,))
+    rot_mat[:, 0, 0] = torch.cos(random_values)
+    rot_mat[:, 0, 1] = -torch.sin(random_values)
+    rot_mat[:, 1, 0] = torch.sin(random_values)
+    rot_mat[:, 1, 1] = torch.cos(random_values)
+    rot_mat[:, 2, 2] = 1
+    return rot_mat
 
-def verbosify(cast_fn, fn_name, verbose):
-    if verbose:
-        return functools.partial(cast_fn, name=fn_name, verbose=verbose)
-    else:
-        return cast_fn
+def repeat_rot_mat(rot_mat, num):
+    batch = rot_mat.shape[0]
+    res = torch.zeros([batch, 3*num, 3*num]).to(rot_mat.device)
+    for i in range(num):
+        res[:, 3*i:3*(i+1), 3*i:3*(i+1)] = rot_mat
+    return res
 
-def as_inplace(fns):
-    for x in fns:
-        yield x + '_'
+def align_skeleton(data):
+    N, C, T, V, M = data.shape
+    trans_data = np.zeros_like(data)
+    for i in tqdm(range(N)):
+        for p in range(M):
+            sample = data[i][..., p]
+            # if np.all((sample[:,0,:] == 0)):
+                # continue
+            d = sample[:,0,1:2]
+            v1 = sample[:,0,1]-sample[:,0,0]
+            if np.linalg.norm(v1) <= 0.0:
+                continue
+            v1 = v1/np.linalg.norm(v1)
+            v2_ = sample[:,0,12]-sample[:,0,16]
+            proj_v2_v1 = np.dot(v1.T,v2_)*v1/np.linalg.norm(v1)
+            v2 = v2_-np.squeeze(proj_v2_v1)
+            v2 = v2/(np.linalg.norm(v2))
+            v3 = np.cross(v2,v1)/(np.linalg.norm(np.cross(v2,v1)))
+            v1 = np.reshape(v1,(3,1))
+            v2 = np.reshape(v2,(3,1))
+            v3 = np.reshape(v3,(3,1))
 
-def has_func(mod, fn):
-    if isinstance(mod, dict):
-        return fn in mod
-    else:
-        return hasattr(mod, fn)
+            R = np.hstack([v2,v3,v1])
+            for t in range(T):
+                trans_sample = (np.linalg.inv(R))@(sample[:,t,:]) # -d
+                trans_data[i, :, t, :, p] = trans_sample
+    return trans_data
 
-def get_func(mod, fn):
-    if isinstance(mod, dict):
-        return mod[fn]
-    else:
-        return getattr(mod, fn)
+def create_aligned_dataset(file_list=['data/ntu/NTU60_CS.npz', 'data/ntu/NTU60_CV.npz']):
+    for file in file_list:
+        org_data = np.load(file)
+        splits = ['x_train', 'x_test']
+        aligned_set = {}
+        for split in splits:
+            data = org_data[split]
+            N, T, _ = data.shape
+            data = data.reshape((N, T, 2, 25, 3)).transpose(0, 4, 1, 3, 2)
+            aligned_data = align_skeleton(data)
+            aligned_data = aligned_data.transpose(0, 2, 4, 3, 1).reshape(N, T, -1)
+            aligned_set[split] = aligned_data
 
-def set_func(mod, fn, new_fn):
-    if isinstance(mod, dict):
-        mod[fn] = new_fn
-    else:
-        setattr(mod, fn, new_fn)
+        np.savez(file.replace('.npz', '_aligned.npz'),
+                 x_train=aligned_set['x_train'],
+                 y_train=org_data['y_train'],
+                 x_test=aligned_set['x_test'],
+                 y_test=org_data['y_test'])
 
-def set_func_save(handle, mod, fn, new_fn):
-    cur_fn = get_func(mod, fn)
-    handle._save_func(mod, fn, cur_fn)
-    set_func(mod, fn, new_fn)
 
-# A couple problems get solved here:
-# - The flat_weight buffer is disconnected from autograd graph,
-#   so the fp16 weights need to be derived from the input weights
-#   to this forward call, not the flat buffer.
-# - The ordering of weights in the flat buffer is...idiosyncratic.
-# First problem is solved with combination of set_ (to set up
-# correct storage) and copy_ (so the fp16 weight derives from the
-# fp32 one in autograd.
-# Second is solved by doing ptr arithmetic on the fp32 weights
-# to derive the correct offset.
-#
-# TODO: maybe this should actually use
-# `torch._cudnn_rnn_flatten_weight`? But then I need to call
-# on first iter and cache the right offsets. Ugh.
-def synthesize_flattened_rnn_weights(fp32_weights,
-                                     fp16_flat_tensor,
-                                     rnn_fn='',
-                                     verbose=False):
-    fp16_weights = []
-    fp32_base_ptr = fp32_weights[0][0].data_ptr()
-    for layer_weights in fp32_weights:
-        fp16_layer_weights = []
-        for w_fp32 in layer_weights:
-            w_fp16 = w_fp32.new().half()
-            offset = (w_fp32.data_ptr() - fp32_base_ptr) // w_fp32.element_size()
-            w_fp16.set_(fp16_flat_tensor.storage(),
-                        offset,
-                        w_fp32.shape)
-            w_fp16.copy_(w_fp32)
-            if verbose:
-                print('Float->Half ({})'.format(rnn_fn))
-            fp16_layer_weights.append(w_fp16)
-        fp16_weights.append(fp16_layer_weights)
-    return fp16_weights
 
-# Roughly same as above, just the `fp32_weights` aren't nested.
-# Code kept separate for readability.
-def new_synthesize_flattened_rnn_weights(fp32_weights,
-                                         fp16_flat_tensor,
-                                         rnn_fn='',
-                                         verbose=False):
-    fp16_weights = []
-    fp32_base_ptr = fp32_weights[0].data_ptr()
-    for w_fp32 in fp32_weights:
-        w_fp16 = w_fp32.new().half()
-        offset = (w_fp32.data_ptr() - fp32_base_ptr) // w_fp32.element_size()
-        w_fp16.set_(fp16_flat_tensor.storage(),
-                    offset,
-                    w_fp32.shape)
-        w_fp16.copy_(w_fp32)
-        if verbose:
-            print('Float->Half ({})'.format(rnn_fn))
-        fp16_weights.append(w_fp16)
-    return fp16_weights
+def get_motion(data, data_format=['x'], use_nonzero_mask=False, rot=False, jittering=False, random_dist=None):
+    N, C, T, V, M = data.size()
+    data = data.permute(0, 4, 2, 3, 1).contiguous().view(N*M, T, V, C)
+
+    # get motion features
+    x = data - data[:,:,0:1,:] # localize
+    if 'v' in data_format:
+        v = x[:,1:,:,:] - x[:,:-1,:,:]
+        v = torch.cat([torch.zeros(N*M, 1, V, C).to(v.device), v], dim=1)
+    if 'a' in data_format:
+        a = v[:,1:,:,:] - v[:,:-1,:,:]
+        a = torch.cat([torch.zeros(N*M, 1, V, C).to(a.device), a], dim=1)
+
+    # reshape x,v for PORT
+    x = x.view(N*M*T, V, C)
+    if 'v' in data_format:
+        v = v.view(N*M*T, V, C)
+    if 'a' in data_format:
+        a = a.view(N*M*T, V, C)
+
+    # apply nonzero mask
+    if use_nonzero_mask:
+        nonzero_mask = x.view(N*M*T, -1).count_nonzero(dim=-1) !=0
+        x = x[nonzero_mask]
+        if 'v' in data_format:
+            v = v[nonzero_mask]
+        if 'a' in data_format:
+            a = a[nonzero_mask]
+
+    # optionally rotate
+    if rot:
+        rot_mat = random_rot_mat(x.shape[0], random_dist).to(x.device)
+        x = x.transpose(1, 2) # (NMT, C, V)
+        x = torch.bmm(rot_mat, x) # rotate
+        x = x.transpose(1, 2) #(NMT, V, C)
+
+        if 'v' in data_format:
+            v = v.transpose(1, 2) # (NMT, C, V)
+            v = torch.bmm(rot_mat, v) # rotate
+            v = v.transpose(1, 2) #(NMT, V, C)
+
+        if 'a' in data_format:
+            a = a.transpose(1, 2) # (NMT, C, V)
+            a = torch.bmm(rot_mat, a) # rotate
+            a = a.transpose(1, 2) #(NMT, V, C)
+
+    if jittering:
+        jit = (torch.rand(x.shape[0], 1, x.shape[-1], device=x.device) - 0.5) / 10
+        x += jit
+
+    output = {'x':x}
+    if 'v' in data_format:
+        output['v'] = v
+    if 'a' in data_format:
+        output['a'] = a
+
+    return output
+
+def get_attn(x, mask= None, similarity='scaled_dot'):
+    if similarity == 'scaled_dot':
+        sqrt_dim = np.sqrt(x.shape[-1])
+        score = torch.bmm(x, x.transpose(1, 2)) / sqrt_dim
+    elif similarity == 'euclidean':
+        score = torch.cdist(x, x)
+
+    if mask is not None:
+        score.masked_fill_(mask.view(score.size()), -float('Inf'))
+
+    attn = F.softmax(score, -1)
+    embd = torch.bmm(attn, x)
+    return embd, attn
+
+def get_vector_property(x):
+    N, C = x.size()
+    x1 = x.unsqueeze(0).expand(N, N, C)
+    x2 = x.unsqueeze(1).expand(N, N, C)
+    x1 = x1.reshape(N*N, C)
+    x2 = x2.reshape(N*N, C)
+    cos_sim = F.cosine_similarity(x1, x2, dim=1, eps=1e-6).view(N, N)
+    cos_sim = torch.triu(cos_sim, diagonal=1).sum() * 2 / (N*(N-1))
+    pdist = (LA.norm(x1-x2, ord=2, dim=1)).view(N, N)
+    pdist = torch.triu(pdist, diagonal=1).sum() * 2 / (N*(N-1))
+    return cos_sim, pdist
+
+
+class BalancedSampler(Sampler[int]):
+
+    data_source: Sized
+    replacement: bool
+
+    def __init__(self, data_source: Sized, args=None) -> None:
+        self.dt = data_source
+        self.args = args
+        self.n_cls = args.num_class
+        self.n_dt = len(self.dt)
+        self.n_per_cls = self.dt.n_per_cls
+        self.n_cls_wise_desired = int(self.n_dt/self.n_cls)
+        self.n_repeat = np.ceil(self.n_cls_wise_desired/np.array(self.n_per_cls)).astype(int)
+        self.n_samples = self.n_cls_wise_desired * self.n_cls
+        self.st_idx_cls = self.dt.csum_n_per_cls[:-1]
+        self.cls_idx = torch.from_numpy(self.st_idx_cls).\
+           unsqueeze(1).expand(self.n_cls, self.n_cls_wise_desired)
+
+    def num_samples(self) -> int:
+        return self.n_samples
+
+    def __iter__(self):
+        batch_rand_perm_lst = list()
+        for i_cls in range(self.n_cls):
+            rand = torch.rand(self.n_repeat[i_cls], self.n_per_cls[i_cls])
+            brp = rand.argsort(dim=-1).reshape(-1)[:self.n_cls_wise_desired]
+            batch_rand_perm_lst.append(brp)
+        batch_rand_perm  = torch.stack(batch_rand_perm_lst, 0)
+        batch_rand_perm += self.cls_idx
+        b = batch_rand_perm.permute(1, 0).reshape(-1).tolist()
+        yield from b
+
+    def __len__(self):
+        return self.num_samples
+
+if __name__ == "__main__":
+    create_aligned_dataset()
